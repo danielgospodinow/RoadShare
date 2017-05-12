6f908b883631f391530e34fa3ca6a1045a70de14
Vladimir Ananiev <vladeto97@gmail.com>
2017-05-12 13:23:42 +0300

    Firebase auth initializing

---
+++ app/src/main/java/com/roadshare/dgospodinow/roadshare/EmailPasswordActivity.java
@@ -0,0 +1,10 @@
+package com.roadshare.dgospodinow.roadshare;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by vladimir on 5/12/17.
+ */
+
+public class EmailPasswordActivity extends AppCompatActivity {
+}
