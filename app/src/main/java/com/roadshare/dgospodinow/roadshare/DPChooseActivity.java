a4ef32971ae9226e9c725269403705d9a44c9c82
danielgospodinow <danielgospodinow@gmail.com>
2017-05-13 13:36:25 +0300

    Finishing DPChooseActivity

--- app/src/main/java/com/roadshare/dgospodinow/roadshare/DPChooseActivity.java
+++ app/src/main/java/com/roadshare/dgospodinow/roadshare/DPChooseActivity.java
@@ -4,16 +4,15 @@ package com.roadshare.dgospodinow.roadshare;
  * Created by dgospodinow on 5/12/17.
  */
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class DPChooseActivity extends AppCompatActivity
@@ -24,8 +23,54 @@ public class DPChooseActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.dp_choose_activity);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setTextViewFont((TextView)findViewById(R.id.textView));
+        setTextViewFont((TextView)findViewById(R.id.textView2));
+
+        Button withCarButton = (Button)findViewById(R.id.button3);
+        final ImageView outterCircle = (ImageView) findViewById(R.id.outterCircle);
+        withCarButton.setOnTouchListener(new View.OnTouchListener()
+        {
+            public boolean onTouch(View view, MotionEvent motionevent)
+            {
+                int action = motionevent.getAction();
+                if (action == MotionEvent.ACTION_DOWN)
+                {
+                    outterCircle.setImageResource(R.drawable.ic_circle_svg3);
+
+                }
+                else if (action == MotionEvent.ACTION_UP)
+                {
+                    outterCircle.setImageResource(R.drawable.ic_circle_svg2);
+                }
+                return false;
+            }
+        });
+
+        Button withoutCarButton = (Button)findViewById(R.id.button2);
+        final ImageView outterCircle2 = (ImageView) findViewById(R.id.outterCircle2);
+        withoutCarButton.setOnTouchListener(new View.OnTouchListener()
+        {
+            public boolean onTouch(View view, MotionEvent motionevent)
+            {
+                int action = motionevent.getAction();
+                if (action == MotionEvent.ACTION_DOWN)
+                {
+                    outterCircle2.setImageResource(R.drawable.ic_circle_svg3);
+
+                }
+                else if (action == MotionEvent.ACTION_UP)
+                {
+                    outterCircle2.setImageResource(R.drawable.ic_circle_svg2);
+                }
+                return false;
+            }
+        });
     }
 
-
+    public void setTextViewFont(TextView t)
+    {
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/Blogger_Sans.otf");
+        t.setTypeface(font);
+    }
 }
